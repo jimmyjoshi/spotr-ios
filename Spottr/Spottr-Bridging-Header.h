de9966276b2136c74367379d3a87634599174545
jimmyjoshi <anujjaha@users.noreply.github.com>
2018-05-23 23:22:59 +0530

    Bridging File

---
+++ Spottr/Spottr-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Spottr-Bridging-Header.h
+//  Spottr
+//
+//  Created by Kevin on 20/05/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+#ifndef Spottr_Bridging_Header_h
+#define Spottr_Bridging_Header_h
+
+#import "ActionSheetPicker.h"
+#import "UIImageView+WebCache.h"
+
+#endif /* Spottr_Bridging_Header_h */
