670c3e9ce7ac538d935423357b42b7a691e8c62a
jimmyjoshi <anujjaha@users.noreply.github.com>
2018-05-26 18:03:03 +0530

    ViewFriends

---
+++ Spottr/Spottr/ViewPost/ViewFriendsVC.swift
@@ -0,0 +1,71 @@
+//
+//  ViewFriendsVC.swift
+//  Spottr
+//
+//  Created by Yash on 26/05/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewFriendsVC: UIViewController
+{
+    @IBOutlet weak var tblFriend : UITableView!
+    var arrFriends = NSMutableArray()
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.tblFriend.estimatedRowHeight = 200
+        self.tblFriend.rowHeight = UITableViewAutomaticDimension
+    }
+
+    @IBAction func backButtonPressed()
+    {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension ViewFriendsVC : UITableViewDelegate,UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return UITableViewAutomaticDimension
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendsTableViewCell") as! FriendsTableViewCell
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 5
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
